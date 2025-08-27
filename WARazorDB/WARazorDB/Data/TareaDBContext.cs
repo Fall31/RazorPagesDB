06d6326cb9c8fd4e09364e06119bc1e101eef8b7
Fall31 <lazomorenowilsonlazo@gmail.com>
2025-08-26 21:03:19 -0400

    segundo com

---
+++ WARazorDB/WARazorDB/Data/TareaDBContext.cs
@@ -0,0 +1,17 @@
+﻿using WARazorDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WARazorDB.Data
+{
+    public class TareaDBContext : DbContext
+    {
+        public TareaDBContext(DbContextOptions<TareaDBContext> options): base(options) 
+        {
+
+        }
+        public DbSet<Tarea> Tareas { get; set; }
+        protected TareaDBContext() 
+        {
+                    }
+    }
+}
